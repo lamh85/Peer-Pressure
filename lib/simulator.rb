12815610f14941e79adc44af1cae1051fe019f77
Hon Lam <lamh85@gmail.com>
2015-10-28 00:47:53 -0700

    Reformatted outputs to conform with SPEC

--- lib/simulator.rb
+++ lib/simulator.rb
@@ -3,29 +3,14 @@ class Simulator
   attr_accessor :seating
 
   def initialize(input)
-    @seating = input.to_s
-
-    # Convert the user input into a 2-dimensional array
-    # ---
-    # Trim whitespaces
-    @seating = @seating.gsub(" ","")
-    # Split only the first dimension
-    @seating = @seating.split("],[")
-    @seating.each_with_index do |element, index|
-        # Remove square brackets
-        @seating[index] = element.gsub(/(\[|\])/,"")
-        # Split the second dimension
-        @seating[index] = @seating[index].split(",").map(&:to_sym)
-    end
+    @seating = input
   end
 
   def verdict
     frequencies = {hard: 0, soft: 0}
     @seating.each do |row|
         row.each do |seat|
-            if frequencies[seat]
-                frequencies[seat] = frequencies[seat] + 1
-            end
+            frequencies[seat] = frequencies[seat] + 1 if frequencies[seat]
         end
     end
     frequencies = Hash[frequencies.sort_by{ |k,v| v }.reverse]
@@ -35,10 +20,11 @@ class Simulator
     elsif frequencies.values[0] == frequencies.values[1]
         verdict = :push
     end
-    puts "The verdict is: #{verdict}"
+    return verdict
   end
 
-  def state
+  # I added this method because I was disappointed that SPEC doesn't want "state" to return pretty formatting =P
+  def state_pretty
     puts "==="
     puts "The current seating is: "
     @seating.each do |row|
@@ -46,6 +32,10 @@ class Simulator
       puts ""
     end
     puts "==="
+  end
+
+  def state
+    return @seating
   end
 
   def next
@@ -104,40 +94,3 @@ class Simulator
   end
 
 end
-
-
-=begin
-
-MECHANISM FOR EVALUATING THE RESULT OF ONE PERSON:
-0: Convert the inputted string into an array
-1: ID the person via index numbers
-2: ID the person's neighbours and return an array
-3: Evaluate the array: find the conditional that fits the description of the array
-
-# If person HAD OPINION
-# ---------------------
-
-# If a person {with an opinion} has {fewer than 2 opinionated neighbours} then they stop having an opinion since there are not enough people to argue with.
-if examined != :none && neighbours_opinionated.count < 2
-  examined = :none
-end
-
-# If a person {with an opinion} has {more than 3 opinionated neighbours} then they stop having an opinion since their opinion is drowned out by their neighbours.
-if examined != :none && neighbours_opinionated.count > 3
-  examined = :none
-end
-
-# If person had NO OPINION + exactly 3 opinionated neighbours
-# -----------------------------------------------------------
-
-# If a person {without an opinion} has {exactly 3 opinionated} neighbours and {at least 2 of them think "Gif" is pronounced with a hard G} then they form an opinion that it is pronounced with a hard G
-if examined == :none && neighbours_opinionated.count == 3 && neighbours_hard >= 2
-  examined = :hard
-end
-
-# If a person {without an opinion} has {exactly 3 opinionated} neighbours and {at least 2 of them think "Gif" is pronounced with a soft G} then they form an opinion that it is pronounced with a soft G
-if examined == :none && neighbours_opinionated.count == 3 && neighbours_soft >= 2
-  examined = :soft
-end
-
-=end
